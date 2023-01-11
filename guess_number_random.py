129ce73f1a9e867e83ae7643c3e526e5c2922f5d
Aakriti <112646543+aa-kriti-29@users.noreply.github.com>
2023-01-11 10:07:55 +0530

    Create guess_number_random.py

---
+++ guess_number_random.py
@@ -0,0 +1,13 @@
+import random
+num=random.randint(0,50)
+print('Number to be guessed is between 0 to 50 \nwill get 5 chances')
+for i in range(1,6):
+  n=int(input('Enter your guess:'))
+  if n==num:
+    print('Correct guess! \nCongratulations')
+  elif n>50:
+    print('Range is 0-50! \nChoose correctly!')
+  else:
+    print('Wrong Guess! \nTry Again \nYou have ',5-i,' chances left.')
+if n!=num:
+  print('Correct Guess was:',num)
